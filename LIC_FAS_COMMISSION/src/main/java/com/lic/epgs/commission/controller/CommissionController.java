f0e81db32ec393afa6468061b7652b71e4d3906e
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-26 18:02:42 +0530

    Updated

--- LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/controller/CommissionController.java
+++ LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/controller/CommissionController.java
@@ -1,42 +1,54 @@
 package com.lic.epgs.commission.controller;
 
-import com.lic.epgs.commission.dto.CommissionDto;
-import com.lic.epgs.commission.dto.CommissionDetailsDto;
-import com.lic.epgs.commission.entity.Commission;
-import com.lic.epgs.commission.entity.CommissionDetailsEntity;
-import com.lic.epgs.commission.entity.CommissionQuestionDetailsTempEntity;
-import com.lic.epgs.commission.entity.CommissionNotesTempEntity;
-import com.lic.epgs.commission.service.CommissionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.lic.epgs.commission.dto.CommonCommissionDto;
+import com.lic.epgs.commission.entity.CommissionNotes;
+import com.lic.epgs.commission.service.CommissionNotesService;
+import com.lic.epgs.commission.service.CommissionService;
 
 @RestController
 @RequestMapping("/commission")
 public class CommissionController {
 
-    @Autowired
-    private CommissionService commissionService;
-
-    @PostMapping("/save")
-    public Commission saveCommission(@RequestBody CommissionDto commissionDto) {
-        return commissionService.saveCommission(commissionDto);
-    }
-
-    @PostMapping("/details/save")
-    public CommissionDetailsEntity saveCommissionDetails(@RequestBody CommissionDetailsDto commissionDetailsDto) {
-        return commissionService.saveCommissionDetails(commissionDetailsDto);
-    }
-
-    @PostMapping("/questiondetails/save")
-    public List<CommissionQuestionDetailsTempEntity> saveCommissionQuestionDetailsTemp(@RequestBody CommissionQuestionDetailsTempEntity commissionQuestionDetailsTempEntity) {
-        return commissionService.saveCommissionQuestionDetailsTemp(commissionQuestionDetailsTempEntity);
-    }
-
-    @PostMapping("/notes/save")
-    public List<CommissionNotesTempEntity> saveCommissionNotesTemp(@RequestBody CommissionNotesTempEntity commissionNotesTempEntity) {
-        return commissionService.saveCommissionNotesTemp(commissionNotesTempEntity);
-    }
-
+	@Autowired
+	private CommissionService commissionService;
+	
+	@Autowired
+	private CommissionNotesService commissionNotesService;
+	
+	@GetMapping("/{status}")
+	public List<CommonCommissionDto> getCommissionByStatus(@PathVariable String status) {
+		return commissionService.getCommissionByStatus(status);
+	}
+	
+	@PutMapping("/{status}/{id}")
+	public int updateStatus(@PathVariable String status, @PathVariable Long id) {
+		return commissionService.updateStatus(status, id);
+	}
+	
+	@GetMapping("/{status}/question-master")
+	public List<CommonCommissionDto> loadQuestionMaster(@PathVariable String status) {
+		return commissionService.loadQuestionMaster(status);
+	}
+	
+	@GetMapping("/note/{commissionId}")
+	public CommissionNotes findByCommissionId(@PathVariable long commissionId) {
+		return commissionNotesService.findByCommissionId(commissionId);
+	}
+	
+	@PutMapping("/note/{commissionId}")
+	public CommissionNotes save(CommissionNotes commissionNotes) {
+		return commissionNotesService.save(commissionNotes);
+	}
+	
+	@PutMapping("/note/{commissionId}/delete")
+	public void deleteByCommissionId(@PathVariable long commissionId) {
+		commissionNotesService.deleteByCommissionId(commissionId);
+	}
 }
