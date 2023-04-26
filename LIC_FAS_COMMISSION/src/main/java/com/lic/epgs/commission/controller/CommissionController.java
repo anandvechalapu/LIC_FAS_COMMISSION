4fc8dc24a71cbedaa0ee00c212b1c1bb6e6e9ecf
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-26 19:04:21 +0530

    Updated

--- LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/controller/CommissionController.java
+++ LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/controller/CommissionController.java
@@ -1,15 +1,18 @@
 package com.lic.epgs.commission.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.commission.dto.CommonCommissionDto;
-import com.lic.epgs.commission.entity.CommissionNotes;
-import com.lic.epgs.commission.service.CommissionNotesService;
+import com.lic.epgs.commission.model.CommissionModel;
+import com.lic.epgs.commission.service.CheckerApproveService;
 import com.lic.epgs.commission.service.CommissionService;
 
 @RestController
@@ -20,35 +23,26 @@ public class CommissionController {
 	private CommissionService commissionService;
 	
 	@Autowired
-	private CommissionNotesService commissionNotesService;
-	
-	@GetMapping("/{status}")
-	public List<CommonCommissionDto> getCommissionByStatus(@PathVariable String status) {
-		return commissionService.getCommissionByStatus(status);
+	private CheckerApproveService checkerApproveService;
+
+	@GetMapping("/details")
+	public ResponseEntity<Object> getCommissionDetails() {
+		return new ResponseEntity<>(commissionService.loadCommissionDetails(), HttpStatus.OK);
 	}
-	
-	@PutMapping("/{status}/{id}")
-	public int updateStatus(@PathVariable String status, @PathVariable Long id) {
-		return commissionService.updateStatus(status, id);
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getCommissionById(@PathVariable Long id) {
+		return new ResponseEntity<>(checkerApproveService.findByCommissionId(id), HttpStatus.OK);
 	}
-	
-	@GetMapping("/{status}/question-master")
-	public List<CommonCommissionDto> loadQuestionMaster(@PathVariable String status) {
-		return commissionService.loadQuestionMaster(status);
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateCommissionStatus(@PathVariable Long id) {
+		return new ResponseEntity<>(checkerApproveService.updateCommissionStatus(id), HttpStatus.OK);
 	}
-	
-	@GetMapping("/note/{commissionId}")
-	public CommissionNotes findByCommissionId(@PathVariable long commissionId) {
-		return commissionNotesService.findByCommissionId(commissionId);
+
+	@PostMapping("/")
+	public ResponseEntity<Object> addCommission(@RequestBody CommissionModel commissionModel) {
+		return new ResponseEntity<>(checkerApproveService.addCommissionToCommissionEntity(commissionModel.getCommissionId()), HttpStatus.CREATED);
 	}
-	
-	@PutMapping("/note/{commissionId}")
-	public CommissionNotes save(CommissionNotes commissionNotes) {
-		return commissionNotesService.save(commissionNotes);
-	}
-	
-	@PutMapping("/note/{commissionId}/delete")
-	public void deleteByCommissionId(@PathVariable long commissionId) {
-		commissionNotesService.deleteByCommissionId(commissionId);
-	}
+
 }
