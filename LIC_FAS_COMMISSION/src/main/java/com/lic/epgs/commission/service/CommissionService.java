7fc4ede527457d472b750e25d219d9894168feb7
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-26 18:00:46 +0530

    Updated

--- LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/service/CommissionService.java
+++ LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/service/CommissionService.java
@@ -1,77 +1,37 @@
 package com.lic.epgs.commission.service;
 
-import java.util.List;
-
+import com.lic.epgs.commission.dto.CommissionDto;
+import com.lic.epgs.commission.dto.CommissionDetailsDto;
+import com.lic.epgs.commission.entity.Commission;
+import com.lic.epgs.commission.entity.CommissionDetailsEntity;
+import com.lic.epgs.commission.entity.CommissionQuestionDetailsTempEntity;
+import com.lic.epgs.commission.entity.CommissionNotesTempEntity;
+import com.lic.epgs.commission.repository.CommissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.commission.model.CommissionTempEntity;
-import com.lic.epgs.commission.model.CommissionQuestionDetailsTempEntity;
-import com.lic.epgs.commission.model.CommissionNotesTempEntity;
-import com.lic.epgs.commission.model.CommissionEntity;
-import com.lic.epgs.commission.model.CommissionQuestionDetailsEntity;
-import com.lic.epgs.commission.model.CommissionNotesEntity;
-import com.lic.epgs.commission.repository.CommissionTempRepository;
-import com.lic.epgs.commission.repository.CommissionQuestionDetailsTempRepository;
-import com.lic.epgs.commission.repository.CommissionNotesTempRepository;
-import com.lic.epgs.commission.repository.CommissionRepository;
-import com.lic.epgs.commission.repository.CommissionQuestionDetailsRepository;
-import com.lic.epgs.commission.repository.CommissionNotesRepository;
+import java.util.List;
 
 @Service
 public class CommissionService {
-    
-    @Autowired
-    private CommissionTempRepository commissionTempRepository;
-    
-    @Autowired
-    private CommissionQuestionDetailsTempRepository commissionQuestionDetailsTempRepository;
-    
-    @Autowired
-    private CommissionNotesTempRepository commissionNotesTempRepository;
-    
+
     @Autowired
     private CommissionRepository commissionRepository;
-    
-    @Autowired
-    private CommissionQuestionDetailsRepository commissionQuestionDetailsRepository;
-    
-    @Autowired
-    private CommissionNotesRepository commissionNotesRepository;
-    
-    public CommissionTempEntity getCommissionTemp(Long commissionId, Boolean isActive) {
-        return commissionTempRepository.findByCommissionIdAndIsActive(commissionId, isActive);
+
+    public Commission saveCommission(CommissionDto commissionDto) throws ConstraintViolationException, PersistenceException {
+        return commissionRepository.saveCommission(commissionDto);
     }
-    
-    public List<CommissionQuestionDetailsTempEntity> getCommissionQuestionDetailsTemp(Long commissionId, Boolean isActive) {
-        return commissionQuestionDetailsTempRepository.findByCommissionIdAndIsActive(commissionId, isActive);
+
+    public CommissionDetailsEntity saveCommissionDetails(CommissionDetailsDto commissionDetailsDto) throws ConstraintViolationException, PersistenceException {
+        return commissionRepository.saveCommissionDetails(commissionDetailsDto);
     }
-    
-    public List<CommissionNotesTempEntity> getCommissionNotesTemp(Long commissionId, Boolean isActive) {
-        return commissionNotesTempRepository.findByCommissionIdAndIsActive(commissionId, isActive);
+
+    public List<CommissionQuestionDetailsTempEntity> saveCommissionQuestionDetailsTemp(CommissionQuestionDetailsTempEntity commissionQuestionDetailsTempEntity) throws ConstraintViolationException, PersistenceException {
+        return commissionRepository.saveCommissionQuestionDetailsTemp(commissionQuestionDetailsTempEntity);
     }
-    
-    public CommissionEntity saveCommission(CommissionEntity commissionEntity) {
-        return commissionRepository.save(commissionEntity);
+
+    public List<CommissionNotesTempEntity> saveCommissionNotesTemp(CommissionNotesTempEntity commissionNotesTempEntity) throws ConstraintViolationException, PersistenceException {
+        return commissionRepository.saveCommissionNotesTemp(commissionNotesTempEntity);
     }
-    
-    public List<CommissionQuestionDetailsEntity> saveCommissionQuestionDetails(List<CommissionQuestionDetailsEntity> commissionQuestionDetailsEntities) {
-        return commissionQuestionDetailsRepository.saveAll(commissionQuestionDetailsEntities);
-    }
-    
-    public List<CommissionNotesEntity> saveCommissionNotes(List<CommissionNotesEntity> commissionNotesEntities) {
-        return commissionNotesRepository.saveAll(commissionNotesEntities);
-    }
-    
-    public CommissionTempEntity saveCommissionTemp(CommissionTempEntity commissionTempEntity) {
-        return commissionTempRepository.save(commissionTempEntity);
-    }
-    
-    public List<CommissionQuestionDetailsTempEntity> saveCommissionQuestionDetailsTemp(List<CommissionQuestionDetailsTempEntity> commissionQuestionDetailsTempEntities) {
-        return commissionQuestionDetailsTempRepository.saveAll(commissionQuestionDetailsTempEntities);
-    }
-    
-    public List<CommissionNotesTempEntity> saveCommissionNotesTemp(List<CommissionNotesTempEntity> commissionNotesTempEntities) {
-        return commissionNotesTempRepository.saveAll(commissionNotesTempEntities);
-    }
+
 }
