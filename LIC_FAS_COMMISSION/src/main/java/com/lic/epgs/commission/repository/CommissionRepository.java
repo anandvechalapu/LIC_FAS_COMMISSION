995c1f1784a390f5e5691a7714173635cee50911
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-26 17:59:02 +0530

    Updated

--- LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/repository/CommissionRepository.java
+++ LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/repository/CommissionRepository.java
@@ -1,30 +1,21 @@
-package com.lic.epgs.commission.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import com.lic.epgs.commission.model.Commission;
-
 @Repository
-public interface CommissionRepository extends JpaRepository<Commission, Long> {
+public interface CommissionRepository extends JpaRepository<CommonCommissionDto, Long>{
 
-    @Query("SELECT c FROM Commission c WHERE c.commissionId = :commissionId")
-    Commission getCommissionById(@Param("commissionId") Long commissionId);
-
-    Commission save(Commission commission);
-
-    Commission saveAndFlush(Commission commission);
-
-    Commission update(Commission commission);
-
-    Commission addCommissionDetails(Commission commission);
-
-    Commission addCommissionNotesAndQuestions(Commission commission);
-
-    void setTransactionMessage(String message);
-
-    void setTransactionStatus(String status);
+	List<CommonCommissionDto> findByStatus(String status);
+	
+	@Transactional
+	@Modifying
+	@Query("UPDATE CommonCommissionDto c SET c.status=:status WHERE c.id=:id")
+	int updateStatus(@Param("status") String status, @Param("id") Long id);
+	
+	@Transactional
+	@Modifying
+	@Query("DELETE FROM CommonCommissionDto c WHERE c.status=:status")
+	int deleteByStatus(@Param("status") String status);
+	
+	@Transactional
+	@Modifying
+	@Query("SELECT c FROM CommonCommissionDto c WHERE c.status=:status")
+	List<CommonCommissionDto> loadQuestionMaster(@Param("status") String status);
 
 }
