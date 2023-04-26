af06cd7418d799672f8747f676c65a9f7602450f
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-26 17:52:09 +0530

    initial commit

---
+++ LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/common/model/CommonControllerCommonStatus.java
@@ -0,0 +1,65 @@
+package com.lic.epgs.common.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "common_controller_common_status")
+public class CommonControllerCommonStatus {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String code;
+	
+	private String type;
+	
+	private String name;
+	
+	private String isActive;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getIsActive() {
+		return isActive;
+	}
+	
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+}
