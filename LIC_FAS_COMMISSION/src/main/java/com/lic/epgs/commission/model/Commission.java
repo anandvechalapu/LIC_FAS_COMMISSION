b3e4d5b30a238ca13432155cb1acbb451005c2c5
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-26 18:00:01 +0530

    Updated

--- LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/model/Commission.java
+++ LIC_FAS_COMMISSION/src/main/java/com/lic/epgs/commission/model/Commission.java
@@ -1,82 +1,64 @@
 package com.lic.epgs.commission.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "commissions")
+@Table(name = "commission")
 public class Commission {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "commission_id")
-    private Long commissionId;
-
-    @Column(name = "commission_details")
-    private String commissionDetails;
-
-    @Column(name = "commission_notes")
-    private String commissionNotes;
-
-    @Column(name = "commission_questions")
-    private String commissionQuestions;
-
-    @Column(name = "transaction_message")
-    private String transactionMessage;
-
-    @Column(name = "transaction_status")
-    private String transactionStatus;
-
-    public Long getCommissionId() {
-        return commissionId;
-    }
-
-    public void setCommissionId(Long commissionId) {
-        this.commissionId = commissionId;
-    }
-
-    public String getCommissionDetails() {
-        return commissionDetails;
-    }
-
-    public void setCommissionDetails(String commissionDetails) {
-        this.commissionDetails = commissionDetails;
-    }
-
-    public String getCommissionNotes() {
-        return commissionNotes;
-    }
-
-    public void setCommissionNotes(String commissionNotes) {
-        this.commissionNotes = commissionNotes;
-    }
-
-    public String getCommissionQuestions() {
-        return commissionQuestions;
-    }
-
-    public void setCommissionQuestions(String commissionQuestions) {
-        this.commissionQuestions = commissionQuestions;
-    }
-
-    public String getTransactionMessage() {
-        return transactionMessage;
-    }
-
-    public void setTransactionMessage(String transactionMessage) {
-        this.transactionMessage = transactionMessage;
-    }
-
-    public String getTransactionStatus() {
-        return transactionStatus;
-    }
-
-    public void setTransactionStatus(String transactionStatus) {
-        this.transactionStatus = transactionStatus;
-    }
-
+	
+	@Id
+	private Long id;
+	
+	private String name;
+	
+	private Double amount;
+	
+	private String type;
+	
+	private Boolean isActive;
+	
+	//Getters and Setters
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	
 }
